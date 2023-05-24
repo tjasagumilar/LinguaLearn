36dab37b476b499a7279fa0ee9d9ca0fc96f6367
VerhovnikTeodor <74742395+VerhovnikTeodor@users.noreply.github.com>
2023-05-24 23:56:14 +0200

    naloge
    naloge

---
+++ client/src/Components/Naloge/Tip1/TipNaloge2.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import './TipNaloge2.css'
+import { Exercise } from './Exercises';
+
+interface TipNaloge2Props {
+  exercise: Exercise
+}
+
+
+const TipNaloge2 = ({exercise}: TipNaloge2Props) => {
+
+  const [selectedWord, setSelectedWord] = useState<string>();
+
+  const handleToggleButton = (word: string) => {
+    setSelectedWord(word);
+  };
+
+
+
+
+const handleSubmit = (e: React.FormEvent) => {
+  e.preventDefault();
+
+  fetch(`http://localhost:4000/prevedi/${selectedWord}`)
+    .then(response => response.json())
+    .then(data => {
+      const translation = data.translation;
+      if (exercise.sentence === translation) {
+        alert('Pravilno!');
+      } else {
+        alert('Napačno!');
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+
+
+return (
+  <form onSubmit={handleSubmit}>
+    <div>
+      <h1>Kako se reče: <div>{exercise.sentence}</div></h1>
+      <br />
+      ------------------------------------------
+      <br />
+      <h3>Razpoložljive besede: </h3>
+      {exercise.availableWords.map((word, index) => (
+        <span
+        key={index}
+        className={`available-word ${word === selectedWord ? 'selected' : ''}`}
+        onClick={() => handleToggleButton(word)}
+      >
+        {word}
+        </span>
+      ))}
+      <br />
+    </div>
+    <br/>
+    <button type="submit">Preveri</button>
+    
+  </form>
+);
+};
+
+export default TipNaloge2;
