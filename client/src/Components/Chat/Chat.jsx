5f62475f15bed2e641fe9264951be97254a6a05a
miha_plemenitas <miha.plemenitas@student.um.si>
2023-05-26 23:20:21 +0200

    Začetek ChatRoom navigacije

--- client/src/Components/Chat/Chat.jsx
+++ client/src/Components/Chat/Chat.jsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/analytics';
-import {useAuthState} from 'react-firebase-hooks/auth';
-import {auth} from "../../Config/firebase";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from "../../Config/firebase";
 import Signin from "../Prijava/Signin/Signin";
 import ChatRoom from "./ChatRoom/ChatRoom";
+import ChatRoomGer from "./ChatRoom/ChatRoomGer";
 import './Chat.css';
 
-
 function Chat() {
+    const [selectedChat, setSelectedChat] = useState("general");
+
+    const handleChatChange = (event) => {
+        setSelectedChat(event.target.value);
+    };
+
+    let selectedChatComponent;
+
+    if (selectedChat === "general") {
+        selectedChatComponent = <ChatRoom />;
+    } else if (selectedChat === "german") {
+        selectedChatComponent = <ChatRoomGer />;
+    }
+
     return (
         <div className="App">
             <header>
                 <h1>
-                  <span role="img" aria-label="speech balloon">
-                    General Chat 💬
-                  </span>
+          <span role="img" aria-label="speech balloon">
+            General Chat 💬
+          </span>
                 </h1>
             </header>
 
             <section>
-                <ChatRoom/>
+                <div className="chat-container">
+                    <div className="chat-selection">
+                        <label htmlFor="chat-select">Select Chat Room: </label>
+                        <select id="chat-select" value={selectedChat} onChange={handleChatChange}>
+                            <option value="general">General</option>
+                            <option value="german">German</option>
+                        </select>
+                    </div>
+
+                    {selectedChatComponent}
+                </div>
             </section>
         </div>
     );
