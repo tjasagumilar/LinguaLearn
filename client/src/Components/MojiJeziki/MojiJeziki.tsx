d7b258a26e52f0e4bb9a591ccf24f73f73b76a8e
tjasag <tjasa.gumilar@student.um.si>
2023-05-28 01:55:35 +0200

    Prikaz izbranih jezikov

--- client/src/Components/MojiJeziki/MojiJeziki.tsx
+++ client/src/Components/MojiJeziki/MojiJeziki.tsx
@@ -1,10 +1,52 @@
-import { Button, Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { auth } from "../../Config/firebase";
+import "./MojiJeziki.css";
 
 const MojiJeziki = () => {
+    const [jeziki, setJeziki] = useState([{ jezik: '', naziv: '', nivo: '', path: '' }]);
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if (user) {
+                //setUid(user.uid);
+                fetch(`http://localhost:4000/mojijeziki?uid=${user.uid}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        setJeziki(data);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            }
+        });
+    }, []);
+
     return (
-        <Container>
-            <a href="/izberijezik"><Button>Dodaj jezik</Button></a>
-        </Container>
+        <div className="jeziki-container">
+            <a href="/izberijezik"><Button className="dodaj-jezik">Dodaj jezik</Button></a>
+            <div>
+                {jeziki.map((item) => (
+                    <Row className="kartice-jezikov">
+                        <Col>
+                            <div className="kartica col-sm-11 col-md-8 col-lg-7 col-xxl-5">
+                                <div className="zastava col-12 col-sm-6  col-lg-5">
+                                    <img src={item.path}></img>
+                                </div>
+                                <div className="podrobnosti col-12 col-sm-6 col-lg-7">
+                                    <div> {item.naziv} - {item.nivo} </div>
+                                    <div className="gumbi">
+                                        <a href="/naloge"><Button>Naloge</Button></a>
+                                        <a href="/napredek"><Button>Več</Button></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                ))}
+
+            </div>
+        </div>
     );
 }
 
