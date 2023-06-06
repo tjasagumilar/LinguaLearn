e4c2290775c78372482e64b83cdcbf4c6bdb3b2e
miha_plemenitas <miha.plemenitas@student.um.si>
2023-06-06 17:17:49 +0200

    Profile CSS

--- client/src/Components/Profil/Profil.tsx
+++ client/src/Components/Profil/Profil.tsx
@@ -1,21 +1,20 @@
 import { Button, Col, Row } from "react-bootstrap";
 import { auth } from "../../Config/firebase";
-import "./Profil.css"
+import "./Profil.css";
 import { useEffect, useState } from "react";
 import { BASE_URL } from "../../api";
 
 const Profil = () => {
-    const [username, setUsername] = useState('');
-    const [ime, setIme] = useState('');
-    const [priimek, setPriimek] = useState('');
-    const [slika, setSlika] = useState('');
-    const [path, setPath] = useState('');
-    const [opis, setOpis] = useState('');
+    const [username, setUsername] = useState("");
+    const [ime, setIme] = useState("");
+    const [priimek, setPriimek] = useState("");
+    const [slika, setSlika] = useState("");
+    const [path, setPath] = useState("");
+    const [opis, setOpis] = useState("");
 
     useEffect(() => {
         auth.onAuthStateChanged(user => {
             if (user) {
-                //setUid(user.uid);
                 fetch(`${BASE_URL}/uporabnik?uid=${user.uid}`)
                     .then(response => response.json())
                     .then(data => {
@@ -30,7 +29,6 @@ const Profil = () => {
                         console.log(error);
                     });
             }
-
         });
     }, []);
 
@@ -39,21 +37,25 @@ const Profil = () => {
             <Row>
                 <Col md>
                     <div>
-                        <img src={path} />
+                        <img src={path} alt="Profile" />
                     </div>
                 </Col>
             </Row>
             <Row>
                 <Col md>
                     <div className="profil-podatki">
-                        <div>{username}<br /> {ime} {priimek} <br/> {opis}</div>
+                        <div className="username">{username}</div>
+                        <div className="ime-priimek">{ime} {priimek}</div>
+                        <div className="opis">{opis}</div>
                     </div>
                 </Col>
             </Row>
             <Row>
                 <Col md>
                     <div>
-                        <a href="/uredi"><Button className="uredi-profil">Uredi profil</Button></a>
+                        <a href="/uredi">
+                            <Button className="uredi-profil">Uredi profil</Button>
+                        </a>
                     </div>
                 </Col>
             </Row>
